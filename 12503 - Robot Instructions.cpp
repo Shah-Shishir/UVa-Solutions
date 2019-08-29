c48f3ecd91e3ac36b01cc38a756dc9165433d45f
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2019-08-30 00:38:41 +0600

    Add files via upload

--- 12503 - Robot Instructions.cpp
+++ 12503 - Robot Instructions.cpp
@@ -1,8 +1,8 @@
 /***
 
             Bismillahir Rahmanir Rahim
-            Read the name of Allah, who created you!!!
-            Author : Shah Newaj Rabbi Shishir
+            Read in the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir,
             Department of CSE, City University, Bangladesh.
 
 ***/
@@ -10,58 +10,107 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define sf scanf
-#define pf printf
-#define scase sf ("%d",&tc)
-#define sn sf ("%d",&n)
-#define whilecase while (tc--)
-#define eof while (cin >> n)
-#define forloop for (pos=1; pos<=tc; pos++)
-#define arrayloop (i=0; i<n; i++)
-#define cinstr cin >> str
-#define getstr getline (cin,str)
-#define pcase pf ("Case %d: ",pos)
-#define pb push_back
-#define in insert
+#define             jeno_joyer_khuda_thake      int main (void)
+#define             kaj_shesh                   return 0
+#define             sf                          scanf
+#define             pf                          printf
+#define             ssf                         sscanf
+#define             spf                         sprintf
+#define             fsf                         fscanf
+#define             fpf                         fprintf
+#define             fast                        ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define             scase                       sf ("%d",&tc)
+#define             whilecase                   while (tc--)
+#define             sn                          sf ("%d",&n)
+#define             eof                         while (cin >> n)
+#define             pcase                       pf ("Case %d: ",pos)
+#define             llu                         unsigned long long
+#define             lld                         long long
+#define             U                           unsigned int
+#define             for0(i,n)                   for (i=0; i<n; i++)
+#define             for1(i,n)                   for (i=1; i<=n; i++)
+#define             forab(i,a,b)                for (i=a; i<=b; i++)
+#define             rof0(i,n)                   for (i=n-1; i>=0; i--)
+#define             rof1(i,n)                   for (i=n; i>=1; i--)
+#define             rofab(i,a,b)                for (i=b; i>=a; i--)
+#define             c(str)                      cin >> str
+#define             g(str)                     	getline (cin,str)
+#define             vi                          vector <int>
+#define             vlld                        vector <lld>
+#define             si                          set <int>
+#define             slld                        set <lld>
+#define             ss                          set <string>
+#define             vs                          vector <string>
+#define             pii                         pair <int,int>
+#define             mii                         map <int,int>
+#define             msi                         map <string,int>
+#define             pb                          push_back
+#define             in                          insert
+#define             B                           begin()
+#define             E                           end()
+#define             rB                          rbegin()
+#define             rE                          rend()
+#define             F                           first
+#define             S                           second
+#define             iterate(it,x)               for (it=x.B; it!=x.E; it++)
+#define             riterate(rit,x)             for (it=x.rB; it!=x.rE; it++)
+#define             sort(x)            	        sort (x.B,x.E)
+#define             sortR(x,a,b)            	sort (x.B+a,x.B+b+1)
+#define             rev(x)                  	reverse (x.B,x.E)
+#define             revR(x,a,b)             	reverse (x.B+a,x.B+b+1)
+#define             cl(x)                       x.clear()
+#define             end1                        "\n"
 
-int main (void)
+const int MOD = 1000000007;
+const int MAX = 1000005;
+const double PI = acos(-1.0);
+
+int SetBit (int n, int X) { return n | (1 << X); }
+int ClearBit (int n, int X) { return n & ~(1 << X); }
+int ToggleBit (int n, int X) { return n ^ (1 << X); }
+bool CheckBit (int n, int X) { return (bool)(n & (1 << X)); }
+
+jeno_joyer_khuda_thake
 {
-    int t,n,len,i,num;
-    string a,b;
+    /*
+		freopen ("input.txt","r",stdin);
+		freopen ("output.txt","w",stdout);
+    */
 
-    cin >> t;
+    int tc,n,i,num;
+    char a[10],b[10];
 
-    while (t--)
+    sf ("%d",&tc);
+
+    while (tc--)
     {
-        cin >> n;
+        sf ("%d",&n);
 
-        bool arr[101];
-        int count = 0;
+        char arr[n+1];
+        int ans = 0;
 
-        for (i=0; i<n; i++)
+        for (i=1; i<=n; i++)
         {
-            cin >> a;
-
-            len = a.size();
+            sf ("%s",a);
 
             if (a[0] == 'L')
-                arr[i] = false;
+                arr[i] = 'L';
             else if (a[0] == 'R')
-                arr[i] = true;
+                arr[i] = 'R';
             else
             {
-                cin >> b >> num;
-                arr[i] = arr[num-1];
+                sf ("%s %d",b,&num);
+                arr[i] = arr[num];
             }
 
-            if (arr[i])
-                count++;
+            if (arr[i] == 'R')
+                ans++;
             else
-                count--;
+                ans--;
         }
 
-        cout << count << endl;
+        pf ("%d\n",ans);
     }
 
-    return 0;
+    kaj_shesh;
 }
